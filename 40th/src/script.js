60ab3237d77f92fb7edc5dfc8335748a47d3059e
Frederique Bordeleau <frederique.bordeleau@cirquedusoleil.com>
2023-11-24 14:29:52 -0500

    orbit controls for each animal prop

--- 40th/src/script.js
+++ 40th/src/script.js
@@ -15,25 +15,45 @@ const objectLoader = new GLTFLoader();
 const canvasChevre = document.querySelector("#canvas-chevre");
 fitCanvaToContainer(canvasChevre);
 
+const canvasLion = document.querySelector("#canvas-lion");
+fitCanvaToContainer(canvasLion);
+
 function fitCanvaToContainer(canvas) {
   canvas.style.width = "100%";
   canvas.style.height = "100%";
-  canvas.width = canvasChevre.offsetWidth;
-  canvas.height = canvasChevre.offsetHeight;
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
 }
 
 // Scene
 const scene = new THREE.Scene();
+const sceneLion = new THREE.Scene();
 
-// 3D Object Loader
+/**
+ * 3D Object Loader
+ */
+
+// CHEVRE
 objectLoader.load(
   "objects/chevre.glb",
   function (chevre) {
-    // console.log(chevre.scene);
     chevre.scene.position.y = -1.5;
     chevre.scene.position.x = 0.25;
-    // chevre.scene.scale.set(0.5, 0.5, 0.5);
     scene.add(chevre.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+// LION
+objectLoader.load(
+  "objects/lion.glb",
+  function (lion) {
+    lion.scene.position.y = -1.5;
+    lion.scene.position.x = 0.25;
+    sceneLion.add(lion.scene);
   },
   undefined,
   function (error) {
@@ -50,6 +70,12 @@ directionalLight.position.set(1, 0, 2);
 const directionalLight2 = new THREE.DirectionalLight("#ffffff", 2);
 directionalLight2.position.set(-1, -1, -5);
 
+const directionalLightLion = new THREE.DirectionalLight("#ffffff", 2);
+directionalLight.position.set(1, 0, 2);
+
+const directionalLight2Lion = new THREE.DirectionalLight("#ffffff", 2);
+directionalLight2.position.set(-1, -1, -5);
+
 // const directionalLightHelper = new THREE.DirectionalLightHelper(
 //   directionalLight2,
 //   0.2
@@ -57,6 +83,7 @@ directionalLight2.position.set(-1, -1, -5);
 // scene.add(directionalLightHelper);
 
 scene.add(directionalLight, directionalLight2);
+sceneLion.add(directionalLightLion, directionalLight2Lion);
 
 /**
  * Sizes
@@ -79,11 +106,23 @@ const camera = new THREE.PerspectiveCamera(35, 1 / 1, 1, 20);
 camera.position.z = 10;
 cameraGroup.add(camera);
 
+// Group Lion
+const cameraGroupLion = new THREE.Group();
+scene.add(cameraGroupLion);
+
+// Base camera Lion
+const cameraLion = new THREE.PerspectiveCamera(35, 1 / 1, 1, 20);
+cameraLion.position.z = 10;
+cameraGroup.add(cameraLion);
+
 /**
  * Controls
  */
 const controls = new OrbitControls(camera, canvasChevre);
 controls.enableDamping = true;
+
+const controlsLion = new OrbitControls(cameraLion, canvasLion);
+controlsLion.enableDamping = true;
 
 /**
  * Renderer
@@ -93,8 +132,14 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true,
 });
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
-// renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const rendererLion = new THREE.WebGLRenderer({
+  canvas: canvasLion,
+  alpha: true,
+});
+rendererLion.outputColorSpace = THREE.LinearSRGBColorSpace;
+rendererLion.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
@@ -109,6 +154,7 @@ const tick = () => {
 
   // Render
   renderer.render(scene, camera);
+  rendererLion.render(sceneLion, cameraLion);
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
