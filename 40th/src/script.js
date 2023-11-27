aad445d8b295bfc5e61553f324a9c64ce43f9c6c
Frederique Bordeleau <frederique.bordeleau@cirquedusoleil.com>
2023-11-27 10:21:22 -0500

    added gui to change light color and added rotation animation to animal heads

--- 40th/src/script.js
+++ 40th/src/script.js
@@ -1,11 +1,23 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as dat from "lil-gui";
+
+let chevreObj = null;
+let lionObj = null;
+
+const gui = new dat.GUI({ width: 300 });
+gui.close();
+const parameters = {
+  lightColor: "#ffffff",
+};
+
+gui.addColor(parameters, "lightColor").onChange(changeLightColor);
 
 THREE.ColorManagement.enabled = false;
-const parameters = {
-  materialColor: "#ffeded",
-};
+// const parameters = {
+//   materialColor: "#ffeded",
+// };
 const objectLoader = new GLTFLoader();
 
 /**
@@ -43,6 +55,7 @@ const sceneLion = new THREE.Scene();
 objectLoader.load(
   "objects/chevre.glb",
   function (chevre) {
+    chevreObj = chevre.scene;
     chevre.scene.position.y = -1.5;
     chevre.scene.position.x = 0.25;
     scene.add(chevre.scene);
@@ -57,6 +70,7 @@ objectLoader.load(
 objectLoader.load(
   "objects/lion.glb",
   function (lion) {
+    lionObj = lion.scene;
     lion.scene.position.y = -1.1;
     lion.scene.scale.set(1.3, 1.3, 1.3);
     sceneLion.add(lion.scene);
@@ -71,18 +85,32 @@ objectLoader.load(
  * Light
  */
 
+// const lightColor = new THREE.Color(parameters.lightColor);
+function changeLightColor() {
+  directionalLight.color.set(parameters.lightColor);
+  directionalLight2.color.set(parameters.lightColor);
+  directionalLightLion.color.set(parameters.lightColor);
+  directionalLight2Lion.color.set(parameters.lightColor);
+}
+
 // CHEVRE
-const directionalLight = new THREE.DirectionalLight("#ffffff", 2);
+const directionalLight = new THREE.DirectionalLight(parameters.lightColor, 2);
 directionalLight.position.set(1, 0, 2);
 
-const directionalLight2 = new THREE.DirectionalLight("#ffffff", 2);
+const directionalLight2 = new THREE.DirectionalLight(parameters.lightColor, 2);
 directionalLight2.position.set(-1, -1, -5);
 
 // LION
-const directionalLightLion = new THREE.DirectionalLight("#ffffff", 2);
+const directionalLightLion = new THREE.DirectionalLight(
+  parameters.lightColor,
+  2
+);
 directionalLightLion.position.set(2, 0, 2);
 
-const directionalLight2Lion = new THREE.DirectionalLight("#ffffff", 2);
+const directionalLight2Lion = new THREE.DirectionalLight(
+  parameters.lightColor,
+  2
+);
 directionalLight2Lion.position.set(-1, -1, -5);
 
 // const directionalLightHelper = new THREE.DirectionalLightHelper(
@@ -160,6 +188,16 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
+
+  // Lion animation
+  if (lionObj) {
+    lionObj.rotation.y = elapsedTime * 0.2;
+  }
+
+  // Chevre animation
+  if (chevreObj) {
+    chevreObj.rotation.y = elapsedTime * 0.2;
+  }
 
   // Render
   renderer.render(scene, camera);
