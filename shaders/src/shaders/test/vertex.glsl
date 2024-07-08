b19d71002d9e8919273f66a628d7c44ffd116d61
Frederique Bordeleau <frederique.bordeleau@cirquedusoleil.com>
2024-07-08 15:28:30 -0400

    shaders exercise

--- shaders/src/shaders/test/vertex.glsl
+++ shaders/src/shaders/test/vertex.glsl
@@ -1,10 +1,25 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
 
-attribute vec3 position;
+uniform vec2 uFrequency;
+uniform float uTime;
+
+
+varying vec2 vUv;
+varying float vElevation;
 
 void main()
 {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    modelPosition.z += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition;
+
+    vUv = uv;
+    vElevation = elevation;
 }
